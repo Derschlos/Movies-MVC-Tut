01855baade97cae6d1c76df0a782be3950b13098
davidleonschmidt <davidleonschmidt@mensching>
2022-11-23 16:57:16 +0100

    Projektdateien hinzufügen.

---
+++ Movies/Models/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Data;
+using System;
+using System.Linq;
+
+
+namespace Movies.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context = new MoviesContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MoviesContext>>()
+                ))
+            {
+                // Look for any movies
+                if (context.MovieModel.Any())
+                {
+                    return; //Db has been seeded
+                }
+
+                context.MovieModel.AddRange(
+                      new MovieModel
+                      {
+                          Title = "When Harry Met Sally",
+                          ReleaseDate = DateTime.Parse("1989-2-12"),
+                          Genre = "Romantic Comedy",
+                          Price = 7.99M
+                      },
+
+                        new MovieModel
+                        {
+                            Title = "Ghostbusters ",
+                            ReleaseDate = DateTime.Parse("1984-3-13"),
+                            Genre = "Comedy",
+                            Price = 8.99M
+                        },
+
+                        new MovieModel
+                        {
+                            Title = "Ghostbusters 2",
+                            ReleaseDate = DateTime.Parse("1986-2-23"),
+                            Genre = "Comedy",
+                            Price = 9.99M
+                        },
+
+                        new MovieModel
+                        {
+                            Title = "Rio Bravo",
+                            ReleaseDate = DateTime.Parse("1959-4-15"),
+                            Genre = "Western",
+                            Price = 3.99M
+                        }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
